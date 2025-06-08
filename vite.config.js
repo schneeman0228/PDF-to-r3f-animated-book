fd7a626abd0f8a7cbfbde2707b8e1cc7321b4d7a
schneeman0228 <schneeman0228@gmail.com>
2025-06-09 01:39:59 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/PDF-to-r3f-animated-book/', // リポジトリ名を追加
   optimizeDeps: {
     include: ['pdfjs-dist']
   },
